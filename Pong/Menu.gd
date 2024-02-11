bd90cc215b2a24039cbb5ae105060e4753c2a56c
cfdavidh <cfdavidfernandez@yahoo.com>
2024-02-11 01:25:17 -0300

    creando el script de la señal del boton multijugador, ahora se crea un host

--- Pong/Menu.gd
+++ Pong/Menu.gd
@@ -1,23 +1,23 @@
 extends Control
-
+var peer
 @export var adrees = "192.0.0.1"
+@export var port = 8910
 
 func _ready():
 	multiplayer.peer_connected.connect(player_connected)
 	multiplayer.peer_disconnected.connect(player_disconnected)
 	multiplayer.connected_to_server.connect(connected_to_server)
 	multiplayer.connection_failed.connect(connection_failed)
+	
 
 func player_connected(id):
 	print("player connected" + id)
-
 
 func player_disconnected(id):
 	print("player diconnected" + id)
 
 func connected_to_server():
 	print("connect to the server")
-
 
 func connection_failed():
 	print("failed to connect to the server")
@@ -28,7 +28,18 @@ func _on_singleplayer_pressed():
 
 
 func _on_host_pressed():
-	pass # Replace with function body.
+	#crea el objeto peer que proviene de Multiplayerpeer 
+	peer = ENetMultiplayerPeer.new()
+	#el objeto creado (peer) lo inicializo como server y ese metodo retorna OK cuando se conecta y un error cuando no se conecta con exito
+	var error = peer.create_server(port, 2)
+	if error != OK:
+		print("can not host: " + error)
+		return
+	#empaqueta los datos enviados (aun no se para que sirva, creo que para reducir el ancho de banda utilizado? o quisas dependa del udp)
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER) 
+	#aca envio mi peer que creó un servidor al MultiplayerAPI 
+	multiplayer.set_multiplayer_peer(peer)
+	print("waiting for players")
 
 
 func _on_join_pressed():
